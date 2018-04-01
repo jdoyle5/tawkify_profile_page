3287841f606ba49cfb7a76ee543eb85eacab0001
Joey Doyle <j.doyle345@gmail.com>
2018-04-01 14:19:33 -0700

    Initial git push

---
+++ frontend/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+const App = () => (
+  <h1>
+    It works!
+  </h1>
+);
+
+export default App;
