9a12183c2cbd5c2e2cac35dc9c02e6a4cbd6a3ab
Joey Doyle <j.doyle345@gmail.com>
2018-04-01 17:16:33 -0700

    Add ideal partner reducer to handle the ideal partner action dispatch

--- frontend/reducers/ideal_partner_reducer.js
+++ frontend/reducers/ideal_partner_reducer.js
@@ -1,2 +1,14 @@
 import merge from 'lodash/merge';
 import { RECIEVE_TOP_QUALITIES } from '../actions/ideal_partner/actions';
+
+const topQualities = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECIEVE_TOP_QUALITIES:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default topQualities;
